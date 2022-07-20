925dd6c6cc76dc9e2118f13d17d07226a39f1ab7
KevinTss <kevin.tassi@gmail.com>
2022-07-20 21:27:32 +0200

    Retrieve body data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('sign-up')
-  signUp() {
+  signUp(@Body() dto: AuthDto) {
+    console.log('dto', dto);
     return this.authService.signUp();
   }
 
