390532c5999d5036998ea7a5bdb2f0d1107026ff
KevinTss <kevin.tassi@gmail.com>
2024-04-01 16:10:22 +0200

    Reset migrations

--- prisma/migrations/20240401125512_init/migration.sql
+++ prisma/migrations/20240401125512_init/migration.sql
@@ -4,6 +4,12 @@ CREATE TYPE "AccountProvider" AS ENUM ('LOCAL', 'GOOGLE', 'FACEBOOK');
 -- CreateEnum
 CREATE TYPE "WeekStart" AS ENUM ('SUNDAY', 'MONDAY', 'SATURDAY');
 
+-- CreateEnum
+CREATE TYPE "AttendenceType" AS ENUM ('VALIDATE', 'WAITING');
+
+-- CreateEnum
+CREATE TYPE "DanceRole" AS ENUM ('LEADER', 'FOLLOWER');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -49,11 +55,42 @@ CREATE TABLE "workspaceConfig" (
 );
 
 -- CreateTable
-CREATE TABLE "WorkspaceUser" (
+CREATE TABLE "Member" (
     "userId" INTEGER NOT NULL,
     "workspaceId" INTEGER NOT NULL,
 
-    CONSTRAINT "WorkspaceUser_pkey" PRIMARY KEY ("userId","workspaceId")
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("userId","workspaceId")
+);
+
+-- CreateTable
+CREATE TABLE "events" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "dateStart" TIMESTAMP(3) NOT NULL,
+    "dateEnd" TIMESTAMP(3),
+    "duration" TEXT NOT NULL,
+    "location" TEXT,
+    "capacityMin" INTEGER,
+    "capacityMax" INTEGER,
+    "leaderOffset" INTEGER DEFAULT 0,
+    "followerOffset" INTEGER DEFAULT 0,
+    "rule" TEXT[],
+    "organizerId" INTEGER NOT NULL,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attendee" (
+    "role" "DanceRole" NOT NULL,
+    "type" "AttendenceType" NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "eventId" INTEGER NOT NULL,
+
+    CONSTRAINT "Attendee_pkey" PRIMARY KEY ("userId","eventId")
 );
 
 -- CreateIndex
@@ -66,7 +103,16 @@ ALTER TABLE "accounts" ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userI
 ALTER TABLE "workspaceConfig" ADD CONSTRAINT "workspaceConfig_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WorkspaceUser" ADD CONSTRAINT "WorkspaceUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WorkspaceUser" ADD CONSTRAINT "WorkspaceUser_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Member" ADD CONSTRAINT "Member_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_organizerId_fkey" FOREIGN KEY ("organizerId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attendee" ADD CONSTRAINT "Attendee_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attendee" ADD CONSTRAINT "Attendee_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
